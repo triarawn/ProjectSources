23a3b3597cb8f7142a5e082eeb4e224620ed3a8b
Triarawn <55967100+triarawn@users.noreply.github.com>
2026-03-04 18:03:06 -0700

    Update GhostGenerator.lua
    Update Ghost Generator to match one currently up on the Marketplace

--- GhostGenerator.lua
+++ GhostGenerator.lua
@@ -1,5 +1,6 @@
 -- Ghost Generator. Generates Ghost Data that you can play back!
 -- Coded by Hexibin, 11/21/2025
+-- Last Updated 3/4/2026
 
 local toolbar = plugin:CreateToolbar("Ghost Generator")
 local recordButton = toolbar:CreateButton("Start Recording", "Start recording a Ghost", "rbxassetid://115273269847299")
@@ -7,19 +8,27 @@ local stopRecording = toolbar:CreateButton("Stop Recording", "Stop recording a G
 
 stopRecording.Enabled = false
 
+type AnimationChunk = { -- A chunk of animation data.
+	ID:string,
+	Speed:number,
+	Priority:string,
+	Weight:number
+}
+
 type PlaybackChunk = { -- A chunk of playback data, used to store movement.
 	Position:CFrame,
-	AnimationsPlaying:{AnimationTrack},
+	AnimationsPlaying:{AnimationChunk},
 	CurrHumState:Enum.HumanoidStateType
 }
 
 local recording = false
 local recording_name = "LastPlaybackData"
 local recording_playback_data : {PlaybackChunk} = {}
+local MAX_SIZE_UNTIL_SPLIT = 200 -- How many chunks until we have to split? 
 
 local SpecialCharacters = {['\a'] = '\\a', ['\b'] = '\\b', ['\f'] = '\\f', ['\n'] = '\\n', ['\r'] = '\\r', ['\t'] = '\\t', ['\v'] = '\\v', ['\0'] = '\\0'}
 local Keywords = { ['and'] = true, ['break'] = true, ['do'] = true, ['else'] = true, ['elseif'] = true, ['end'] = true, ['false'] = true, ['for'] = true, ['function'] = true, ['if'] = true, ['in'] = true, ['local'] = true, ['nil'] = true, ['not'] = true, ['or'] = true, ['repeat'] = true, ['return'] = true, ['then'] = true, ['true'] = true, ['until'] = true, ['while'] = true, ['continue'] = true}
-local Functions = {[DockWidgetPluginGuiInfo.new] = "DockWidgetPluginGuiInfo.new"; [warn] = "warn"; [CFrame.fromMatrix] = "CFrame.fromMatrix"; [CFrame.fromAxisAngle] = "CFrame.fromAxisAngle"; [CFrame.fromOrientation] = "CFrame.fromOrientation"; [CFrame.fromEulerAnglesXYZ] = "CFrame.fromEulerAnglesXYZ"; [CFrame.Angles] = "CFrame.Angles"; [CFrame.fromEulerAnglesYXZ] = "CFrame.fromEulerAnglesYXZ"; [CFrame.new] = "CFrame.new"; [gcinfo] = "gcinfo"; [os.clock] = "os.clock"; [os.difftime] = "os.difftime"; [os.time] = "os.time"; [os.date] = "os.date"; [tick] = "tick"; [bit32.band] = "bit32.band"; [bit32.extract] = "bit32.extract"; [bit32.bor] = "bit32.bor"; [bit32.bnot] = "bit32.bnot"; [bit32.arshift] = "bit32.arshift"; [bit32.rshift] = "bit32.rshift"; [bit32.rrotate] = "bit32.rrotate"; [bit32.replace] = "bit32.replace"; [bit32.lshift] = "bit32.lshift"; [bit32.lrotate] = "bit32.lrotate"; [bit32.btest] = "bit32.btest"; [bit32.bxor] = "bit32.bxor"; [pairs] = "pairs"; [NumberSequence.new] = "NumberSequence.new"; [assert] = "assert"; [tonumber] = "tonumber"; [Color3.fromHSV] = "Color3.fromHSV"; [Color3.toHSV] = "Color3.toHSV"; [Color3.fromRGB] = "Color3.fromRGB"; [Color3.new] = "Color3.new"; [Delay] = "Delay"; [Stats] = "Stats"; [UserSettings] = "UserSettings"; [coroutine.resume] = "coroutine.resume"; [coroutine.yield] = "coroutine.yield"; [coroutine.running] = "coroutine.running"; [coroutine.status] = "coroutine.status"; [coroutine.wrap] = "coroutine.wrap"; [coroutine.create] = "coroutine.create"; [coroutine.isyieldable] = "coroutine.isyieldable"; [NumberRange.new] = "NumberRange.new"; [PhysicalProperties.new] = "PhysicalProperties.new"; [PluginManager] = "PluginManager"; [Ray.new] = "Ray.new"; [NumberSequenceKeypoint.new] = "NumberSequenceKeypoint.new"; [Version] = "Version"; [Vector2.new] = "Vector2.new"; [Instance.new] = "Instance.new"; [delay] = "delay"; [spawn] = "spawn"; [unpack] = "unpack"; [string.split] = "string.split"; [string.match] = "string.match"; [string.gmatch] = "string.gmatch"; [string.upper] = "string.upper"; [string.gsub] = "string.gsub"; [string.format] = "string.format"; [string.lower] = "string.lower"; [string.sub] = "string.sub"; [string.pack] = "string.pack"; [string.rep] = "string.rep"; [string.char] = "string.char"; [string.packsize] = "string.packsize"; [string.reverse] = "string.reverse"; [string.byte] = "string.byte"; [string.unpack] = "string.unpack"; [string.len] = "string.len"; [string.find] = "string.find"; [CellId.new] = "CellId.new"; [ypcall] = "ypcall"; [version] = "version"; [print] = "print"; [stats] = "stats"; [printidentity] = "printidentity"; [settings] = "settings"; [UDim2.fromOffset] = "UDim2.fromOffset"; [UDim2.fromScale] = "UDim2.fromScale"; [UDim2.new] = "UDim2.new"; [table.pack] = "table.pack"; [table.move] = "table.move"; [table.insert] = "table.insert"; [table.getn] = "table.getn"; [table.foreachi] = "table.foreachi"; [table.maxn] = "table.maxn"; [table.foreach] = "table.foreach"; [table.concat] = "table.concat"; [table.unpack] = "table.unpack"; [table.find] = "table.find"; [table.create] = "table.create"; [table.sort] = "table.sort"; [table.remove] = "table.remove"; [TweenInfo.new] = "TweenInfo.new"; [loadstring] = "loadstring"; [require] = "require"; [Vector3.FromNormalId] = "Vector3.FromNormalId"; [Vector3.FromAxis] = "Vector3.FromAxis"; [Vector3.fromAxis] = "Vector3.fromAxis"; [Vector3.fromNormalId] = "Vector3.fromNormalId"; [Vector3.new] = "Vector3.new"; [Vector3int16.new] = "Vector3int16.new"; [setmetatable] = "setmetatable"; [next] = "next"; [Wait] = "Wait"; [wait] = "wait"; [ipairs] = "ipairs"; [elapsedTime] = "elapsedTime"; [time] = "time"; [rawequal] = "rawequal"; [Vector2int16.new] = "Vector2int16.new"; [collectgarbage] = "collectgarbage"; [newproxy] = "newproxy"; [Spawn] = "Spawn"; [PluginDrag.new] = "PluginDrag.new"; [Region3.new] = "Region3.new"; [utf8.offset] = "utf8.offset"; [utf8.codepoint] = "utf8.codepoint"; [utf8.nfdnormalize] = "utf8.nfdnormalize"; [utf8.char] = "utf8.char"; [utf8.codes] = "utf8.codes"; [utf8.len] = "utf8.len"; [utf8.graphemes] = "utf8.graphemes"; [utf8.nfcnormalize] = "utf8.nfcnormalize"; [xpcall] = "xpcall"; [tostring] = "tostring"; [rawset] = "rawset"; [PathWaypoint.new] = "PathWaypoint.new"; [DateTime.fromUnixTimestamp] = "DateTime.fromUnixTimestamp"; [DateTime.now] = "DateTime.now"; [DateTime.fromIsoDate] = "DateTime.fromIsoDate"; [DateTime.fromUnixTimestampMillis] = "DateTime.fromUnixTimestampMillis"; [DateTime.fromLocalTime] = "DateTime.fromLocalTime"; [DateTime.fromUniversalTime] = "DateTime.fromUniversalTime"; [Random.new] = "Random.new"; [typeof] = "typeof"; [RaycastParams.new] = "RaycastParams.new"; [math.log] = "math.log"; [math.ldexp] = "math.ldexp"; [math.rad] = "math.rad"; [math.cosh] = "math.cosh"; [math.random] = "math.random"; [math.frexp] = "math.frexp"; [math.tanh] = "math.tanh"; [math.floor] = "math.floor"; [math.max] = "math.max"; [math.sqrt] = "math.sqrt"; [math.modf] = "math.modf"; [math.pow] = "math.pow"; [math.atan] = "math.atan"; [math.tan] = "math.tan"; [math.cos] = "math.cos"; [math.sign] = "math.sign"; [math.clamp] = "math.clamp"; [math.log10] = "math.log10"; [math.noise] = "math.noise"; [math.acos] = "math.acos"; [math.abs] = "math.abs"; [math.sinh] = "math.sinh"; [math.asin] = "math.asin"; [math.min] = "math.min"; [math.deg] = "math.deg"; [math.fmod] = "math.fmod"; [math.randomseed] = "math.randomseed"; [math.atan2] = "math.atan2"; [math.ceil] = "math.ceil"; [math.sin] = "math.sin"; [math.exp] = "math.exp"; [getfenv] = "getfenv"; [pcall] = "pcall"; [ColorSequenceKeypoint.new] = "ColorSequenceKeypoint.new"; [ColorSequence.new] = "ColorSequence.new"; [type] = "type"; [Region3int16.new] = "Region3int16.new"; [ElapsedTime] = "ElapsedTime"; [select] = "select"; [getmetatable] = "getmetatable"; [rawget] = "rawget"; [Faces.new] = "Faces.new"; [Rect.new] = "Rect.new"; [BrickColor.Blue] = "BrickColor.Blue"; [BrickColor.White] = "BrickColor.White"; [BrickColor.Yellow] = "BrickColor.Yellow"; [BrickColor.Red] = "BrickColor.Red"; [BrickColor.Gray] = "BrickColor.Gray"; [BrickColor.palette] = "BrickColor.palette"; [BrickColor.New] = "BrickColor.New"; [BrickColor.Black] = "BrickColor.Black"; [BrickColor.Green] = "BrickColor.Green"; [BrickColor.Random] = "BrickColor.Random"; [BrickColor.DarkGray] = "BrickColor.DarkGray"; [BrickColor.random] = "BrickColor.random"; [BrickColor.new] = "BrickColor.new"; [setfenv] = "setfenv"; [UDim.new] = "UDim.new"; [Axes.new] = "Axes.new"; [error] = "error"; [debug.traceback] = "debug.traceback"; [debug.profileend] = "debug.profileend"; [debug.profilebegin] = "debug.profilebegin"}
+local Functions = {[CFrame.new] = "CFrame.new";}
 
 function GetHierarchy(Object)
 	local Hierarchy = {}
@@ -60,10 +69,6 @@ local function SerializeType(Value, Class)
 		return GetHierarchy(Value)
 	elseif type(Value) ~= Class then -- CFrame, Vector3, UDim2, ...
 		return Class .. '.new(' .. tostring(Value) .. ')'
-	elseif Class == 'function' then
-		return Functions[Value] or '\'[Unknown ' .. (pcall(setfenv, Value, getfenv(Value)) and 'Lua' or 'C')  .. ' ' .. tostring(Value) .. ']\''
-	elseif Class == 'userdata' then
-		return 'newproxy(' .. tostring(not not getmetatable(Value)) .. ')'
 	elseif Class == 'thread' then
 		return '\'' .. tostring(Value) ..  ', status: ' .. coroutine.status(Value) .. '\''
 	else -- thread, number, boolean, nil, ...
@@ -162,21 +167,6 @@ recordButton.Click:Connect(function()
 	end
 end)
 
-local insert = table.insert
-local move = table.move
-
-function table_split( t, max_size )
-	local result = {}
-
-	for i = 1, #t, max_size do
-		local tn = {}
-		insert(result, tn)
-		move(t, i, i + max_size, 1, tn)
-	end
-
-	return result
-end
-
 stopRecording.Click:Connect(function()
 	-- Check if we're recording
 	if recording == true then
@@ -192,14 +182,15 @@ stopRecording.Click:Connect(function()
 			module_script.Name = name
 			module_script.Parent = game.ReplicatedFirst
 		end
+	
 		
-		if #recording_playback_data > 400 then
+		if #recording_playback_data > MAX_SIZE_UNTIL_SPLIT then
 			local c = 0
 			for i,v in pairs(recording_playback_data) do
 				c += 1
-				if c > 400 then
+				if c > MAX_SIZE_UNTIL_SPLIT then
 					-- we need to split
-					c = c - 400
+					c = c - MAX_SIZE_UNTIL_SPLIT
 					splits += 1
 					SplitName = recording_name .. "_split" .. splits
 					table.insert(current_split_data, recording_name .. "_split" .. splits + 1)
@@ -210,7 +201,7 @@ stopRecording.Click:Connect(function()
 					table.insert(current_split_data, v)
 				end
 			end
-			if #current_split_data < 400 then
+			if #current_split_data < MAX_SIZE_UNTIL_SPLIT then
 				SplitName = recording_name .. "_split" .. splits + 1
 				CreateModuleScript(SplitName, current_split_data)
 			end
@@ -239,7 +230,22 @@ while true do
 				local anims = char.Humanoid.Animator:GetPlayingAnimationTracks()
 				local state : Enum.HumanoidStateType = char.Humanoid:GetState()
 				for i,v : AnimationTrack in pairs(anims) do
-					table.insert(tracks, {v.Animation.AnimationId, v.Speed})
+					local animchunk : AnimationChunk = {
+						ID = v.Animation.AnimationId -- ID will always be there, that's the one thing we need.
+					}
+					if (v.Priority ~= Enum.AnimationPriority.Core) then
+						-- If our Priority is Core, don't save it.
+						animchunk.Priority = v.Priority.Name
+					end
+					if (v.WeightCurrent ~= 1) then
+						-- If our Weight equals one, don't save it.
+						animchunk.Weight = v.WeightCurrent
+					end
+					if (v.Speed ~= 1) then
+						-- If our Speed equals one, don't save it.
+						animchunk.Speed = v.Speed
+					end
+					table.insert(tracks, animchunk)
 				end
 				local chunk : PlaybackChunk = {
 					Position=char.PrimaryPart.CFrame,
